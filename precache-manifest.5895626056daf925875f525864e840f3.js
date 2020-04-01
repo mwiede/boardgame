821008a91babcb1bc41490107ae29f1467b13e39
Wiedemann Matthias <matthias.wiedemann@swisslife.de>
2020-04-01 23:15:12 +0200

    Updates

---
+++ precache-manifest.5895626056daf925875f525864e840f3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2de6469bee09f24d46c7af6da2a3ea",
+    "url": "/boardgame/index.html"
+  },
+  {
+    "revision": "4344a70f808c074c6a4b",
+    "url": "/boardgame/static/css/main.4e0273e1.chunk.css"
+  },
+  {
+    "revision": "f349003b60b639b14b71",
+    "url": "/boardgame/static/js/2.8d2f60bb.chunk.js"
+  },
+  {
+    "revision": "4344a70f808c074c6a4b",
+    "url": "/boardgame/static/js/main.76217aa5.chunk.js"
+  },
+  {
+    "revision": "ea0e218e0b2ced2ceebe",
+    "url": "/boardgame/static/js/runtime~main.18b18355.js"
+  },
+  {
+    "revision": "eb70ae8020121455c5dd71046cd0fc78",
+    "url": "/boardgame/static/media/footprint.eb70ae80.svg"
+  },
+  {
+    "revision": "b377319002474861caaab5a3f6bdf9eb",
+    "url": "/boardgame/static/media/hide.b3773190.svg"
+  },
+  {
+    "revision": "11938bf5b33238748e1eff6f6e40e3d9",
+    "url": "/boardgame/static/media/ladder.11938bf5.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/boardgame/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6fa3765408aed76fd94a3bbf6777b483",
+    "url": "/boardgame/static/media/shuffle.6fa37654.svg"
+  }
+]);
